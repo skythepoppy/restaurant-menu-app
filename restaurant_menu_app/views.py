dc3053b0adc0a61c914f02af9410dda496932487
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2023-08-21 15:58:23 -0500

    implement context data into index html

--- restaurant_menu_app/views.py
+++ restaurant_menu_app/views.py
@@ -5,6 +5,10 @@ class MenuList(generic.ListView):
     queryset = Item.objects.order_by("-date_created")
     template_name = "index.html"
 
+    def get_context_data(self):
+        context = {'meals': ['pizza','pasta'], 'ingredients':['things']}
+
+        return context
 
 class MenuItemDetail(generic.DetailView):
     model = Item

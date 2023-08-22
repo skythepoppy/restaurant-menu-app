1d3f16632cf35900d4086e1a59c904560e712827
skythepoppy <raphaelsebastien.evangelista@gmail.com>
2023-08-22 15:02:30 -0500

    added dynamic links

--- restaurant_menu_app/urls.py
+++ restaurant_menu_app/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from .import views
 
 urlpatterns = [
-    path("", views.MenuList.as_view(), name='home')  # renders class as view
+    path("", views.MenuList.as_view(), name='home'),
+    path('item/<int:pk>/', views.MenuItemDetail.as_view(), name="menu_item")
 ]
